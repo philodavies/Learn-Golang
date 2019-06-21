fd4ef4e15e12cb9862eefecead7faeb889fc24bb
philodavies <philodavies@users.noreply.github.com>
2019-06-21 17:56:02 +0100

    learning about variable references

---
+++ variable_reference.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string ="Joe Jonas"
+    var age int =27
+
+    fmt.Println(name, "is",  age)
+
+    changeName(name)
+
+    fmt.Println(name, "is",  age)
+
+    realChangeName(&name)
+
+    fmt.Println(name, "is",  age)
+}
+
+func changeName(name string) {
+    name = "Doof Jordan"
+
+    fmt.Println(name, "has changed")
+}
+
+
+func realChangeName(name *string) string {
+    *name = "Doof Jordan"
+
+    fmt.Println(*name, "has changed")
+
+    return *name
+}
